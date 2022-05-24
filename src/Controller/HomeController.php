a65455b225e16a90061563d9da2048e8171e04fc
unknown <ala.johan@gmail.com>
2022-05-24 16:55:05 +0200

    squirrel

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,15 +2,19 @@
 
 namespace App\Controller;
 
+use App\Repository\SquirrelRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'index')]
-    public function index(): Response
+    public function index(SquirrelRepository $squirrelRepository): Response
     {
-        return $this->render('home/index.html.twig');
+        return $this->render('home/index.html.twig', [
+            'squirrels' => $squirrelRepository->findAll()
+        ]);
     }
 }
--- src/Entity/Nut.php
+++ src/Entity/Nut.php
@@ -2,11 +2,11 @@
 
 namespace App\Entity;
 
-use App\Repository\NutsRepository;
+use App\Repository\NutRepository;
 use Doctrine\ORM\Mapping as ORM;
 
-#[ORM\Entity(repositoryClass: NutsRepository::class)]
-class Nuts
+#[ORM\Entity(repositoryClass: NutRepository::class)]
+class Nut
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
